372b92e716a3c542af44198cb4a3bf2e28bbcc76
blair909 <61510475+blair909@users.noreply.github.com>
2020-05-15 11:00:36 -0500

    More Data Layer Changes

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -11,19 +11,18 @@ namespace ElevenNote.Data
     {
         [Key]
         public int NoteId { get; set; }
-
         [Required]
         public Guid OwnerId { get; set; }
-
         [Required]
+        [Display(Name = "Note Title")]
         public string Title { get; set; }
-
         [Required]
+        [Display(Name = "Note Content")]
         public string Content { get; set; }
-
         [Required]
+        [Display(Name = "Date Created")]
         public DateTimeOffset CreatedUtc { get; set; }
-
+        [Display(Name = "Date Modified")]
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
